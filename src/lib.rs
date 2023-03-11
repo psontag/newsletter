26169cfe7edcc2b2ab9b94115eb4d5144f90a50e
Philipp Sontag <philipp.sontag@web.de>
2023-03-11 20:51:48 +0100

    Add subscribe route

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,25 @@ use std::net::TcpListener;
 
 use actix_web::{dev::Server, web, App, HttpResponse, HttpServer, Responder};
 
+#[derive(serde::Deserialize)]
+struct SubscriptionInfo {
+    name: String,
+    email: String,
+}
+
 async fn health() -> impl Responder {
     HttpResponse::Ok()
 }
+async fn subscribe(form: web::Form<SubscriptionInfo>) -> impl Responder {
+    HttpResponse::Ok()
+}
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health", web::get().to(health)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health", web::get().to(health))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
     Ok(server)
 }
--- tests/health.rs
+++ tests/health.rs
@@ -1,9 +1,10 @@
 use std::net::TcpListener;
 
+use actix_web::error;
+
 #[tokio::test]
 async fn health_check_works() {
     let address = spawn_app();
-
     let client = reqwest::Client::new();
 
     let response = client
@@ -23,4 +24,46 @@ fn spawn_app() -> String {
 
     let _ = tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
+}
+
+#[tokio::test]
+async fn subscribe_returns_200_for_valid_form_data() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "name=Jon%20Snow&email=jon.snow%40mail.com";
+
+    let response = client
+        .post(&format!("{}/subscriptions", &address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed request");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_400_for_missing_form_data() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let tests = vec![
+        ("", "missing both name and email"),
+        ("name=Jon%20Snow", "missing email"),
+        ("email=jon.snow@mail.com", "missing name"),
+    ];
+
+    for (invalid_form, error_msg) in tests {
+        let response = client
+            .post(&format!("{}/subscriptions", &address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_form)
+            .send()
+            .await
+            .expect("Failed request");
+
+        assert_eq!(400, response.status().as_u16(), "No 400 with {}", error_msg);
+    }
 }
