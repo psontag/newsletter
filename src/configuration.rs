ea38897b17d1d410d6e98043f9069abc9e6ae3bd
Philipp Sontag <philipp.sontag@web.de>
2023-03-11 21:28:26 +0100

    Add configuration

---
+++ src/configuration.rs
@@ -0,0 +1,25 @@
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub port: u16,
+    pub database: DatabaseSettings,
+}
+
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub name: String,
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let settings = config::Config::builder()
+        .add_source(config::File::new(
+            "configuration.yaml",
+            config::FileFormat::Yaml,
+        ))
+        .build()?;
+
+    settings.try_deserialize::<Settings>()
+}
